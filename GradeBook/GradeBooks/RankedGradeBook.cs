6a7b355e25bbe9904832c9538ee68849702648c3
IpsosCSharpPath <41908173+IpsosCSharpPath@users.noreply.github.com>
2018-08-07 17:55:00 +0100

    Override LetterGrade inRankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,11 +5,38 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-   public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade <= 20)
+            {
+                return 'A';
+            }
+            else if (averageGrade > 20 && averageGrade <= 40)
+            {
+                return 'B';
+            }
+            else if (averageGrade > 40 && averageGrade <= 60)
+            {
+                return 'C';
+            }
+            else if (averageGrade > 60 && averageGrade <= 80)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
